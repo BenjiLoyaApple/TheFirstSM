00e538c9aa5223c5f1ec5d6330c71f92c4ef3314
Benji Loya <113779387+BenjiLoyaApple@users.noreply.github.com>
2022-12-12 19:18:37 +0500

    Initial Commit

---
+++ TheFirstSM/TheFirstSMApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheFirstSMApp.swift
+//  TheFirstSM
+//
+//  Created by Benji Loya on 12/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct TheFirstSMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
