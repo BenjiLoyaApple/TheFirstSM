2c17b924055aed133cecccaa6a4c5d410838665e
Benji Loya <113779387+BenjiLoyaApple@users.noreply.github.com>
2022-12-15 21:24:45 +0500

    add view

---
+++ TheFirstSM/View/SwiftUIView.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIView.swift
+//  TheFirstSM
+//
+//  Created by Benji Loya on 15/12/2022.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .foregroundColor(.red)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
