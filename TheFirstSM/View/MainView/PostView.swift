d890a1b9ac11140bbc4ded0185a5aaa7c880de3e
Benji Loya <113779387+BenjiLoyaApple@users.noreply.github.com>
2022-12-27 12:18:47 +0500

    Step 4: add profile card

--- TheFirstSM/View/MainView/PostView.swift
+++ TheFirstSM/View/MainView/PostView.swift
@@ -8,26 +8,31 @@
 import SwiftUI
 
 struct PostView: View {
+    @State private var recentsPosts: [Post] = []
     @State private var createNewPost: Bool = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .hAlign(.center).vAlign(.center)
-            .overlay(alignment: .bottomTrailing){
-        Button {
-            createNewPost.toggle()
-        } label: {
-            Image(systemName: "plus")
-                .font(.title3)
-                .fontWeight(.semibold)
-                .foregroundColor(.white)
-                .padding(14)
-                .background(.black,in: Circle())
-        }
-        .padding(15)
+        NavigationStack {
+            ReusablePostsView(posts: $recentsPosts)
+                .hAlign(.center).vAlign(.center)
+                .overlay(alignment: .bottomTrailing){
+            Button {
+                createNewPost.toggle()
+            } label: {
+                Image(systemName: "plus")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .padding(14)
+                    .background(.black,in: Circle())
             }
+            .padding(15)
+                }
+                .navigationTitle("Post's")
+        }
             .fullScreenCover(isPresented: $createNewPost){
                 CreateNewPost { post in
-                    
+                    /// - Adding Created post at the Top of the Recent posts
+                    recentsPosts.insert(post, at: 0)
                 }
             }
     }
